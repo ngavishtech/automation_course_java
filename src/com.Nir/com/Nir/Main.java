92bfcea7dec066c32d14c02f57ed2c93c186cec0
Nir Gavish <ngavish.tech@gmail.com>
2021-02-09 11:30:49 +0200

    Update Main.java

--- src/com.Nir/com/Nir/Main.java
+++ src/com.Nir/com/Nir/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is my first line :)");
+    	System.out.println("This is my new line");
     }
 }
